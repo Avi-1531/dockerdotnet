c1e6b3f3d4562da9dd3f33c484b0ba7e89fe39c3
Khalid Alshammari <khalidshammari@outlook.sa>
2024-10-23 13:07:11 +0300

    Add main form

---
+++ UI/frmMain.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace UI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.btnUsers = new System.Windows.Forms.Button();
+            this.btnPayments = new System.Windows.Forms.Button();
+            this.btnDepartments = new System.Windows.Forms.Button();
+            this.btnReceptionists = new System.Windows.Forms.Button();
+            this.btnDoctors = new System.Windows.Forms.Button();
+            this.btnAppointments = new System.Windows.Forms.Button();
+            this.btnPatients = new System.Windows.Forms.Button();
+            this.btnPeople = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnlSideBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.pnlSideBar.Controls.Add(this.btnUsers);
+            this.pnlSideBar.Controls.Add(this.btnPayments);
+            this.pnlSideBar.Controls.Add(this.btnDepartments);
+            this.pnlSideBar.Controls.Add(this.btnReceptionists);
+            this.pnlSideBar.Controls.Add(this.btnDoctors);
+            this.pnlSideBar.Controls.Add(this.btnAppointments);
+            this.pnlSideBar.Controls.Add(this.btnPatients);
+            this.pnlSideBar.Controls.Add(this.btnPeople);
+            this.pnlSideBar.Controls.Add(this.btnDashboard);
+            this.pnlSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSideBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(348, 1041);
+            this.pnlSideBar.TabIndex = 0;
+            // 
+            // btnUsers
+            // 
+            this.btnUsers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnUsers.FlatAppearance.BorderSize = 0;
+            this.btnUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsers.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsers.Location = new System.Drawing.Point(0, 877);
+            this.btnUsers.Name = "btnUsers";
+            this.btnUsers.Size = new System.Drawing.Size(348, 78);
+            this.btnUsers.TabIndex = 9;
+            this.btnUsers.Text = "Users";
+            this.btnUsers.UseVisualStyleBackColor = false;
+            this.btnUsers.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // btnPayments
+            // 
+            this.btnPayments.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnPayments.FlatAppearance.BorderSize = 0;
+            this.btnPayments.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPayments.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPayments.Location = new System.Drawing.Point(0, 793);
+            this.btnPayments.Name = "btnPayments";
+            this.btnPayments.Size = new System.Drawing.Size(348, 78);
+            this.btnPayments.TabIndex = 8;
+            this.btnPayments.Text = "Payments";
+            this.btnPayments.UseVisualStyleBackColor = false;
+            this.btnPayments.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // btnDepartments
+            // 
+            this.btnDepartments.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnDepartments.FlatAppearance.BorderSize = 0;
+            this.btnDepartments.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDepartments.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDepartments.Location = new System.Drawing.Point(0, 709);
+            this.btnDepartments.Name = "btnDepartments";
+            this.btnDepartments.Size = new System.Drawing.Size(348, 78);
+            this.btnDepartments.TabIndex = 7;
+            this.btnDepartments.Text = "Departments";
+            this.btnDepartments.UseVisualStyleBackColor = false;
+            this.btnDepartments.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // btnReceptionists
+            // 
+            this.btnReceptionists.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnReceptionists.FlatAppearance.BorderSize = 0;
+            this.btnReceptionists.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReceptionists.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReceptionists.Location = new System.Drawing.Point(0, 625);
+            this.btnReceptionists.Name = "btnReceptionists";
+            this.btnReceptionists.Size = new System.Drawing.Size(348, 78);
+            this.btnReceptionists.TabIndex = 6;
+            this.btnReceptionists.Text = "Receptionists";
+            this.btnReceptionists.UseVisualStyleBackColor = false;
+            this.btnReceptionists.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // btnDoctors
+            // 
+            this.btnDoctors.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnDoctors.FlatAppearance.BorderSize = 0;
+            this.btnDoctors.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDoctors.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoctors.Location = new System.Drawing.Point(0, 541);
+            this.btnDoctors.Name = "btnDoctors";
+            this.btnDoctors.Size = new System.Drawing.Size(348, 78);
+            this.btnDoctors.TabIndex = 5;
+            this.btnDoctors.Text = "Doctors";
+            this.btnDoctors.UseVisualStyleBackColor = false;
+            this.btnDoctors.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // btnAppointments
+            // 
+            this.btnAppointments.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnAppointments.FlatAppearance.BorderSize = 0;
+            this.btnAppointments.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAppointments.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAppointments.Location = new System.Drawing.Point(0, 457);
+            this.btnAppointments.Name = "btnAppointments";
+            this.btnAppointments.Size = new System.Drawing.Size(348, 78);
+            this.btnAppointments.TabIndex = 4;
+            this.btnAppointments.Text = "Appointments";
+            this.btnAppointments.UseVisualStyleBackColor = false;
+            this.btnAppointments.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // btnPatients
+            // 
+            this.btnPatients.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnPatients.FlatAppearance.BorderSize = 0;
+            this.btnPatients.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPatients.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPatients.Location = new System.Drawing.Point(0, 373);
+            this.btnPatients.Name = "btnPatients";
+            this.btnPatients.Size = new System.Drawing.Size(348, 78);
+            this.btnPatients.TabIndex = 3;
+            this.btnPatients.Text = "Patients";
+            this.btnPatients.UseVisualStyleBackColor = false;
+            this.btnPatients.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // btnPeople
+            // 
+            this.btnPeople.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnPeople.FlatAppearance.BorderSize = 0;
+            this.btnPeople.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPeople.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPeople.Location = new System.Drawing.Point(0, 289);
+            this.btnPeople.Name = "btnPeople";
+            this.btnPeople.Size = new System.Drawing.Size(348, 78);
+            this.btnPeople.TabIndex = 2;
+            this.btnPeople.Text = "People";
+            this.btnPeople.UseVisualStyleBackColor = false;
+            this.btnPeople.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.Location = new System.Drawing.Point(0, 205);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(348, 78);
+            this.btnDashboard.TabIndex = 1;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            this.btnDashboard.Click += new System.EventHandler(this._ShowNotImplementedFeatureMessage);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(348, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(1556, 1041);
+            this.pnlMain.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlSideBar);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMain";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlSideBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Button btnUsers;
+        private System.Windows.Forms.Button btnPayments;
+        private System.Windows.Forms.Button btnDepartments;
+        private System.Windows.Forms.Button btnReceptionists;
+        private System.Windows.Forms.Button btnDoctors;
+        private System.Windows.Forms.Button btnAppointments;
+        private System.Windows.Forms.Button btnPatients;
+        private System.Windows.Forms.Button btnPeople;
+        private System.Windows.Forms.Panel pnlMain;
+    }
+}
---
+++ UI/frmMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class frmMain : Form
+    {
+        private void LoadForm(Form frm)
+        {
+
+            pnlMain.Controls.Clear();
+
+            frm.TopLevel = false;
+            frm.Dock = DockStyle.Fill;
+
+            pnlMain.Controls.Add(frm);
+            pnlMain.Tag = frm;
+            frm.Show();
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+
+        }
+
+        private void _ShowNotImplementedFeatureMessage(object sender, EventArgs e)
+        {
+            MessageBox.Show("This feature is not implemented yet.", "Feature Not Available",
+                  MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+        }
+    }
+}
